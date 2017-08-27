e2bd25864225636354fa3dc3300fdbb6ad8641d5
Tara Whiteley <tarawhiteley@gmail.com>
2017-08-27 16:57:17 -0400

    adds form php structure

---
+++ form/error.inc.php
@@ -0,0 +1,60 @@
+<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">
+<link rel="stylesheet" href="../css/animsition.css">
+<link rel="stylesheet" href="../css/vendor/normalize.min.css">
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+
+<link rel="stylesheet" href="../css/main.css">
+
+<div class="animsition" data-animsition-in-class="fade-in"
+data-animsition-in-duration="1000">
+
+
+<nav role="navigation">
+<ul class="nav">
+  <li><a href="../index.html" class="animsition-link bigfont" data-animsition-in-class="fade-in"
+data-animsition-in-duration="1000">Home</a></li>
+
+  <li><a href="../about.html" class="animsition-link bigfont" data-animsition-in-class="fade-in"
+data-animsition-in-duration="1000">About</a></li>
+
+  <li><a href="../specialties.html" class="animsition-link bigfont" data-animsition-in-class="fade-in"
+data-animsition-in-duration="1000">Specialties</a></li>
+
+  <li><a href="../cv.html" class="animsition-link bigfont" data-animsition-in-class="fade-in"
+data-animsition-in-duration="1000">Curriculum Vitae</a></li>
+
+  <li><a href="../news.html" class="animsition-link bigfont" data-animsition-in-class="fade-in"
+data-animsition-in-duration="1000">In The News</a></li>
+
+  <li><a href="../otherservices.html" class="animsition-link bigfont" data-animsition-in-class="fade-in"
+data-animsition-in-duration="1000">Other Services</a></li>
+
+  <li><a href="../contact.html" class="animsition-link bigfont" data-animsition-in-class="fade-in"
+data-animsition-in-duration="1000">Contact</a></li>
+</ul>
+</nav>
+
+<article>
+		<h1>Missing fields</h1>
+		<h3>Sorry, you have not completed all of the required fields.</h3>
+		<h3>Please hit <a href="#" onClick="history.go(-1)">back</a> and complete the required fields.</h3>
+
+		<h3><strong><a href="#" onClick="history.go(-1)">Back to form</a></strong></h3>
+	</article>
+
+  <footer class="clear">
+      <p>Page last updated September 2017.</p>
+  </footer>
+
+    <!-- scripts -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <!-- Latest compiled and minified JavaScript -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <script src="../js/animsition.js"></script>
+    <script src="../js/main.js"></script>
+  </div>
+  </body>
+  </html>
